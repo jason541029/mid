d32548f8b7fe63c52dbee25fb80dbcdceb6985d8
Jason541029 <jtr541029@gmail.com>
2017-11-14 15:23:58 +0800

    Lucky

---
+++ src/com/fju/mid/Lucky.java
@@ -0,0 +1,11 @@
+package com.fju.mid;
+import java.util.Random;
+public class Lucky {
+
+	public static void main(String[] args) {
+		Random ran = new Random();
+		System.out.println();
+        
+	}
+
+}
